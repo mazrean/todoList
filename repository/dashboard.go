abeb6e513bb93b067cc8462faa5d7df267c39487
mazrean <shunsuke.w04@gmail.com>
2021-11-23 01:22:25 +0900

    dashboard repository

---
+++ repository/dashboard.go
@@ -0,0 +1,18 @@
+package repository
+
+//go:generate mockgen -source=$GOFILE -destination=mock/${GOFILE} -package=mock
+
+import (
+	"context"
+
+	"github.com/mazrean/todoList/domain"
+	"github.com/mazrean/todoList/domain/values"
+)
+
+type Dashboard interface {
+	CreateDashboard(ctx context.Context, dashboard *domain.Dashboard) error
+	UpdateDashboard(ctx context.Context, dashboard *domain.Dashboard) error
+	DeleteDashboard(ctx context.Context, id values.DashboardID) error
+	GetMyDashboards(ctx context.Context, userID values.UserID) ([]*domain.Dashboard, error)
+	GetDashboard(ctx context.Context, id values.DashboardID, lockType LockType) (*domain.Dashboard, error)
+}
