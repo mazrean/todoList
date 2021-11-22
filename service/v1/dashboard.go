23f316586b718be81bfa0e48feaf44f6e9dddfc2
mazrean <shunsuke.w04@gmail.com>
2021-11-23 02:29:12 +0900

    UpdateDashboard

--- service/v1/dashboard.go
+++ service/v1/dashboard.go
@@ -2,12 +2,14 @@ package v1
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/mazrean/todoList/domain"
 	"github.com/mazrean/todoList/domain/values"
 	"github.com/mazrean/todoList/repository"
+	"github.com/mazrean/todoList/service"
 )
 
 type Dashboard struct {
@@ -62,6 +64,35 @@ func (d *Dashboard) CreateDashboard(ctx context.Context, user *domain.User, name
 			if err != nil {
 				return fmt.Errorf("failed to create task status: %w", err)
 			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed in transaction: %w", err)
+	}
+
+	return dashboard, nil
+}
+
+func (d *Dashboard) UpdateDashboard(ctx context.Context, id values.DashboardID, name values.DashboardName, description values.DashboardDescription) (*domain.Dashboard, error) {
+	var dashboard *domain.Dashboard
+	err := d.db.Transaction(ctx, nil, func(context.Context) error {
+		var err error
+		dashboard, err = d.dashboardRepository.GetDashboard(ctx, id, repository.LockTypeRecord)
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return service.ErrNoDashboard
+		}
+		if err != nil {
+			return fmt.Errorf("failed to get dashboard: %w", err)
+		}
+
+		dashboard.SetName(name)
+		dashboard.SetDescription(description)
+
+		err = d.dashboardRepository.UpdateDashboard(ctx, dashboard)
+		if err != nil {
+			return fmt.Errorf("failed to update dashboard: %w", err)
 		}
 
 		return nil
