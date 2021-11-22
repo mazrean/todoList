3c44c5e93a607ef9ec9b7599355c847b2211f0cc
mazrean <shunsuke.w04@gmail.com>
2021-11-23 00:08:13 +0900

    dashboad value object

--- domain/values/dashboard.go
+++ domain/values/dashboard.go
@@ -7,3 +7,19 @@ type (
 	DashboardName string
 	DashboardDescription string
 )
+
+func NewDashboardID() DashboardID {
+	return DashboardID(uuid.New())
+}
+
+func NewDashboadIDFromUUID(id uuid.UUID) DashboardID {
+	return DashboardID(id)
+}
+
+func NewDashboardName(name string) DashboardName {
+	return DashboardName(name)
+}
+
+func NewDashboardDescription(description string) DashboardDescription {
+	return DashboardDescription(description)
+}
