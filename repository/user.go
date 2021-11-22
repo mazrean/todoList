0f7d9c2a04195fbcd58fcf706836fd823d686fb2
mazrean <shunsuke.w04@gmail.com>
2021-11-23 01:38:29 +0900

    Signup

--- repository/user.go
+++ repository/user.go
@@ -14,4 +14,5 @@ type User interface {
 	UpdateUser(ctx context.Context, user *domain.User) error
 	DeleteUser(ctx context.Context, id values.UserID) error
 	GetUser(ctx context.Context, userID values.UserID, lockType LockType) (*domain.User, error)
+	GetUserByName(ctx context.Context, name values.UserName) (*domain.User, error)
 }
--- service/v1/authorization.go
+++ service/v1/authorization.go
@@ -1,6 +1,15 @@
 package service
 
-import "github.com/mazrean/todoList/repository"
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/mazrean/todoList/domain"
+	"github.com/mazrean/todoList/domain/values"
+	"github.com/mazrean/todoList/repository"
+	"github.com/mazrean/todoList/service"
+)
 
 type Authorization struct {
 	db             repository.DB
@@ -12,4 +21,40 @@ func NewAuthorization(db repository.DB, userRepository repository.User) *Authori
 		db:             db,
 		userRepository: userRepository,
 	}
+}
+
+func (a *Authorization) Signup(ctx context.Context, name values.UserName, password values.UserPassword) (*domain.User, error) {
+	var user *domain.User
+	err := a.db.Transaction(ctx, nil, func(ctx context.Context) error {
+		_, err := a.userRepository.GetUserByName(ctx, name)
+		if err == nil {
+			return service.ErrUserAlreadyExists
+		}
+		if err != nil && !errors.Is(err, repository.ErrRecordNotFound) {
+			return fmt.Errorf("failed to get user: %w", err)
+		}
+
+		hashedPassword, err := password.Hash()
+		if err != nil {
+			return fmt.Errorf("failed to hash password: %w", err)
+		}
+
+		user = domain.NewUser(
+			values.NewUserID(),
+			name,
+			hashedPassword,
+		)
+
+		err = a.userRepository.CreateUser(ctx, user)
+		if err != nil {
+			return fmt.Errorf("failed to create user: %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed in transaction: %w", err)
+	}
+
+	return user, nil
 }
