f2b717ef037cf309f0f86081ce49688b8ddd2225
mazrean <shunsuke.w04@gmail.com>
2021-11-23 00:06:50 +0900

    task value object

--- domain/values/task.go
+++ domain/values/task.go
@@ -10,3 +10,31 @@ type (
 	TaskStatusID   uuid.UUID
 	TaskStatusName string
 )
+
+func NewTaskID() TaskID {
+	return TaskID(uuid.New())
+}
+
+func NewTaskIDFromUUID(id uuid.UUID) TaskID {
+	return TaskID(id)
+}
+
+func NewTaskName(name string) TaskName {
+	return TaskName(name)
+}
+
+func NewTaskDescription(description string) TaskDescription {
+	return TaskDescription(description)
+}
+
+func NewTaskStatusID() TaskStatusID {
+	return TaskStatusID(uuid.New())
+}
+
+func NewTaskStatusIDFromUUID(id uuid.UUID) TaskStatusID {
+	return TaskStatusID(id)
+}
+
+func NewTaskStatusName(name string) TaskStatusName {
+	return TaskStatusName(name)
+}
