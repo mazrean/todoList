1637e59a3f3b9bda53e433fa754767d091984455
mazrean <shunsuke.w04@gmail.com>
2021-11-23 00:04:58 +0900

    user value object

--- domain/values/user.go
+++ domain/values/user.go
@@ -1,10 +1,61 @@
 package values
 
-import "github.com/google/uuid"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type (
 	UserID uuid.UUID
 	UserName string
-	UserPassword string
-	UserHashedPassword string
+	UserPassword []byte
+	UserHashedPassword []byte
 )
+
+func NewUserID() UserID {
+	return UserID(uuid.New())
+}
+
+func NewUserIDFromUUID(id uuid.UUID) UserID {
+	return UserID(id)
+}
+
+func NewUserName(name string) UserName {
+	return UserName(name)
+}
+
+func NewUserPassword(password []byte) UserPassword {
+	return UserPassword(password)
+}
+
+func (up UserPassword) Hash() (UserHashedPassword, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(up), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	return UserHashedPassword(hashedPassword), nil
+}
+
+func NewUserHashedPassword(hashedPassword []byte) UserHashedPassword {
+	return UserHashedPassword(hashedPassword)
+}
+
+var (
+	ErrInvalidPassword = errors.New("invalid password")
+)
+
+func (uhp UserHashedPassword) Compare(password []byte) error {
+	err := bcrypt.CompareHashAndPassword([]byte(uhp), password)
+	if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+		return ErrInvalidPassword
+	}
+	if err != nil {
+		return fmt.Errorf("failed to compare password: %w", err)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mazrean/todoList
 
 go 1.17
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
+)
