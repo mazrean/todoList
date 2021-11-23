8ffc9b25123c83d89e91eaa90cc57948e0d0f458
mazrean <shunsuke.w04@gmail.com>
2021-11-23 18:50:40 +0900

    GetUser

--- repository/sqlx/user.go
+++ repository/sqlx/user.go
@@ -2,12 +2,22 @@ package sqlx
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/mazrean/todoList/domain"
 	"github.com/mazrean/todoList/domain/values"
+	"github.com/mazrean/todoList/repository"
 )
+
+type UsersTable struct {
+	ID             uuid.UUID    `db:"id"`
+	Name           string       `db:"name"`
+	HashedPassword []byte       `db:"hashed_password"`
+	DeletedAt      sql.NullTime `db:"deleted_at"`
+}
 
 type User struct {
 	db *DB
@@ -47,7 +57,7 @@ func (u *User) UpdateUser(ctx context.Context, user *domain.User) error {
 
 	_, err = db.ExecContext(
 		ctx,
-		"UPDATE users SET name = ?, hashed_password = ? WHERE id = ?",
+		"UPDATE users SET name = ?, hashed_password = ? WHERE id = ? AND deleted_at IS NULL",
 		string(user.GetName()),
 		[]byte(user.GetHashedPassword()),
 		uuid.UUID(user.GetID()),
@@ -67,7 +77,8 @@ func (u *User) DeleteUser(ctx context.Context, id values.UserID) error {
 
 	_, err = db.ExecContext(
 		ctx,
-		"UPDATE users SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?",
+		"UPDATE users SET deleted_at = ? WHERE id = ? AND deleted_at IS NULL",
+		time.Now(),
 		uuid.UUID(id),
 	)
 	if err != nil {
@@ -75,4 +86,30 @@ func (u *User) DeleteUser(ctx context.Context, id values.UserID) error {
 	}
 
 	return nil
+}
+
+func (u *User) GetUser(ctx context.Context, userID values.UserID, lockType repository.LockType) (*domain.User, error) {
+	db, err := u.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	var userTable UsersTable
+	err = db.GetContext(
+		ctx,
+		&userTable,
+		"SELECT id, name, hashed_password FROM users WHERE id = ? AND deleted_at IS NULL",
+		uuid.UUID(userID),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %w", err)
+	}
+
+	user := domain.NewUser(
+		values.NewUserIDFromUUID(userTable.ID),
+		values.NewUserName(userTable.Name),
+		values.NewUserHashedPassword(userTable.HashedPassword),
+	)
+
+	return user, nil
 }
