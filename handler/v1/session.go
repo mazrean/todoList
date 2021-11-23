7518b764f849813ff50e9e1b1cde0ed8425b5730
mazrean <shunsuke.w04@gmail.com>
2021-11-23 22:57:00 +0900

    user

---
+++ handler/v1/session.go
@@ -0,0 +1,124 @@
+package v1
+
+import (
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mazrean/todoList/common"
+	"github.com/mazrean/todoList/domain"
+	"github.com/mazrean/todoList/domain/values"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	sessionContextKey = "session"
+)
+
+type Session struct {
+	key    string
+	secret string
+	store  sessions.Store
+}
+
+func NewSession(key common.SessionKey, secret common.SessionSecret) *Session {
+	store := cookie.NewStore([]byte(secret))
+
+	/*
+		gorilla/sessionsの内部で使われているgobが
+		time.Time,uuid.UUIDのエンコード・デコードをできるようにRegisterする
+	*/
+	gob.Register(time.Time{})
+	gob.Register(uuid.UUID{})
+
+	return &Session{
+		key:    string(key),
+		secret: string(secret),
+		store:  store,
+	}
+}
+
+func (s *Session) Use(r *gin.Engine) {
+	r.Use(sessions.Sessions("session", s.store))
+}
+
+func (s *Session) getSession(c *gin.Context) sessions.Session {
+	session := sessions.Default(c)
+
+	c.Set(sessionContextKey, session)
+
+	return session
+}
+
+func (s *Session) save(c *gin.Context, session sessions.Session) error {
+	err := session.Save()
+	if err != nil {
+		return fmt.Errorf("failed to save session: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Session) revoke(session sessions.Session) {
+	session.Clear()
+}
+
+var (
+	ErrNoValue     = errors.New("no value")
+	ErrValueBroken = errors.New("value broken")
+)
+
+const (
+	userIDSessionKey             = "userID"
+	userNameSessionKey           = "userName"
+	userHashedPasswordSessionKey = "userHashedPassword"
+)
+
+func (s *Session) setUser(session sessions.Session, user *domain.User) {
+	session.Set(userIDSessionKey, uuid.UUID(user.GetID()))
+	session.Set(userNameSessionKey, string(user.GetName()))
+	session.Set(userHashedPasswordSessionKey, []byte(user.GetHashedPassword()))
+}
+
+func (s *Session) getUser(session sessions.Session) (*domain.User, error) {
+	iUserID := session.Get(userIDSessionKey)
+	if iUserID == nil {
+		return nil, ErrNoValue
+	}
+
+	userID, ok := session.Get(userIDSessionKey).(uuid.UUID)
+	if !ok {
+		return nil, ErrValueBroken
+	}
+
+	iUserName := session.Get(userNameSessionKey)
+	if iUserName == nil {
+		return nil, ErrNoValue
+	}
+
+	userName, ok := iUserName.(string)
+	if !ok {
+		return nil, ErrValueBroken
+	}
+
+	iUserHashedPassword := session.Get(userHashedPasswordSessionKey)
+	if iUserHashedPassword == nil {
+		return nil, ErrNoValue
+	}
+
+	userHashedPassword, ok := iUserHashedPassword.([]byte)
+	if !ok {
+		return nil, ErrValueBroken
+	}
+
+	return domain.NewUser(
+		values.NewUserIDFromUUID(userID),
+		values.NewUserName(userName),
+		values.NewUserHashedPassword(userHashedPassword),
+	), nil
+}
---
+++ handler/v1/user.go
@@ -0,0 +1,223 @@
+package v1
+
+import (
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mazrean/todoList/domain/values"
+	"github.com/mazrean/todoList/service"
+)
+
+type User struct {
+	session              *Session
+	authorizationService service.Authorization
+}
+
+func NewUser(
+	session *Session,
+	authorizationService service.Authorization,
+) *User {
+	return &User{
+		session:              session,
+		authorizationService: authorizationService,
+	}
+}
+
+type UserInfo struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+func (u *User) PostSignup(c *gin.Context) {
+	var userInfo UserInfo
+	err := c.BindJSON(&userInfo)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	user, err := u.authorizationService.Signup(
+		c.Request.Context(),
+		values.NewUserName(userInfo.Name),
+		values.NewUserPassword([]byte(userInfo.Password)),
+	)
+	if errors.Is(err, service.ErrUserAlreadyExists) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "user already exists",
+		})
+		return
+	}
+	if err != nil {
+		log.Printf("failed to signup: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to signup",
+		})
+		return
+	}
+
+	session := u.session.getSession(c)
+	u.session.setUser(session, user)
+	err = u.session.save(c, session)
+	if err != nil {
+		log.Printf("failed to save session: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to save session",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "success",
+	})
+}
+
+func (u *User) PostLogin(c *gin.Context) {
+	var userInfo UserInfo
+	err := c.BindJSON(&userInfo)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	user, err := u.authorizationService.Login(
+		c.Request.Context(),
+		values.NewUserName(userInfo.Name),
+		values.NewUserPassword([]byte(userInfo.Password)),
+	)
+	if errors.Is(err, service.ErrInvalidUserOrPassword) {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "invalid user or password",
+		})
+		return
+	}
+	if err != nil {
+		log.Printf("failed to login: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to login",
+		})
+		return
+	}
+
+	session := u.session.getSession(c)
+	u.session.setUser(session, user)
+	err = u.session.save(c, session)
+	if err != nil {
+		log.Printf("failed to save session: %v\n", err)
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "success",
+	})
+}
+
+func (u *User) GetMe(c *gin.Context) {
+	session := u.session.getSession(c)
+	user, err := u.session.getUser(session)
+	if err != nil {
+		log.Printf("failed to get user: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to get user",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, &UserInfo{
+		Name: string(user.GetName()),
+	})
+}
+
+func (u *User) PatchMe(c *gin.Context) {
+	var userInfo UserInfo
+	err := c.BindJSON(&userInfo)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	session := u.session.getSession(c)
+	user, err := u.session.getUser(session)
+	if err != nil {
+		log.Printf("failed to get user: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to get user",
+		})
+		return
+	}
+
+	user, err = u.authorizationService.UpdateUserInfo(
+		c.Request.Context(),
+		user,
+		values.NewUserName(userInfo.Name),
+		values.NewUserPassword([]byte(userInfo.Password)),
+	)
+	if err != nil {
+		log.Printf("failed to update user: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to update user",
+		})
+	}
+
+	u.session.setUser(session, user)
+	err = u.session.save(c, session)
+	if err != nil {
+		log.Printf("failed to save session: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to save session",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+	})
+}
+
+func (u *User) DeleteMe(c *gin.Context) {
+	session := u.session.getSession(c)
+	user, err := u.session.getUser(session)
+	if err != nil {
+		log.Printf("failed to get user: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to get user",
+		})
+		return
+	}
+
+	err = u.authorizationService.DeleteUser(
+		c.Request.Context(),
+		user,
+	)
+	if err != nil {
+		log.Printf("failed to delete user: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to delete user",
+		})
+		return
+	}
+
+	u.session.revoke(session)
+	err = u.session.save(c, session)
+	if err != nil {
+		log.Printf("failed to save session: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to save session",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+	})
+}
