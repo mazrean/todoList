33cf7c27b6845d4d96b05f0ed60fc488366a9aa5
mazrean <shunsuke.w04@gmail.com>
2021-11-23 22:59:16 +0900

    user

--- handler/v1/user.go
+++ handler/v1/user.go
@@ -11,7 +11,7 @@ import (
 )
 
 type User struct {
-	session *Session
+	session              *Session
 	authorizationService service.Authorization
 }
 
@@ -20,13 +20,13 @@ func NewUser(
 	authorizationService service.Authorization,
 ) *User {
 	return &User{
-		session: session,
+		session:              session,
 		authorizationService: authorizationService,
 	}
 }
 
 type UserInfo struct {
-	Name string `json:"name"`
+	Name     string `json:"name"`
 	Password string `json:"password"`
 }
 
