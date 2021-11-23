b2e7039f9ca23fdc8e81007ec7f7f68db7937d83
mazrean <shunsuke.w04@gmail.com>
2021-11-23 15:40:03 +0900

    UpdateTaskStatus

--- service/v1/task_status.go
+++ service/v1/task_status.go
@@ -54,3 +54,33 @@ func (ts *TaskStatus) AddTaskStatus(ctx context.Context, dashboardID values.Dash
 
 	return taskStatus, err
 }
+
+func (ts *TaskStatus) UpdateTaskStatus(ctx context.Context, id values.TaskStatusID, name values.TaskStatusName) (*domain.TaskStatus, error) {
+	var taskStatus *domain.TaskStatus
+	err := ts.db.Transaction(ctx, nil, func(ctx context.Context) error {
+		_, err := ts.taskStatusRepository.GetTaskStatus(ctx, id, repository.LockTypeRecord)
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return service.ErrNoTaskStatus
+		}
+		if err != nil {
+			return fmt.Errorf("failed to get task status: %w", err)
+		}
+
+		taskStatus = domain.NewTaskStatus(
+			id,
+			name,
+		)
+
+		err = ts.taskStatusRepository.UpdateTaskStatus(ctx, taskStatus)
+		if err != nil {
+			return fmt.Errorf("failed to update task status: %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed in transaction: %w", err)
+	}
+
+	return taskStatus, err
+}
