fc6920a19458ea140868705c9242f2653ac0825b
mazrean <shunsuke.w04@gmail.com>
2021-11-23 18:05:40 +0900

    CreateTask

--- service/v1/task.go
+++ service/v1/task.go
@@ -1,6 +1,13 @@
 package v1
 
-import "github.com/mazrean/todoList/repository"
+import (
+	"context"
+	"errors"
+
+	"github.com/mazrean/todoList/domain"
+	"github.com/mazrean/todoList/domain/values"
+	"github.com/mazrean/todoList/repository"
+)
 
 type Task struct {
 	db                   repository.DB
@@ -21,4 +28,16 @@ func NewTask(
 		taskStatusRepository: taskStatusRepository,
 		taskRepository:       taskRepository,
 	}
+}
+
+func (t *Task) CreateTask(ctx context.Context, taskStatusID values.TaskStatusID, name values.TaskName, description values.TaskDescription) (*domain.Task, error) {
+	err := t.db.Transaction(ctx, nil, func(ctx context.Context) error {
+		_, err := t.taskStatusRepository.GetTaskStatus(ctx, taskStatusID, repository.LockTypeRecord)
+		if errors.Is(err, repository.ErrRecordNotFound) {
+			return errors.New("task status not found")
+		}
+		if err != nil {
+			return err
+		}
+	})
 }
