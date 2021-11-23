e60ea7c1af835b91eac753153aaf39f4067b32b1
mazrean <shunsuke.w04@gmail.com>
2021-11-23 18:05:55 +0900

    CreateTask

--- service/v1/task.go
+++ service/v1/task.go
@@ -3,6 +3,8 @@ package v1
 import (
 	"context"
 	"errors"
+	"fmt"
+	"time"
 
 	"github.com/mazrean/todoList/domain"
 	"github.com/mazrean/todoList/domain/values"
@@ -31,13 +33,33 @@ func NewTask(
 }
 
 func (t *Task) CreateTask(ctx context.Context, taskStatusID values.TaskStatusID, name values.TaskName, description values.TaskDescription) (*domain.Task, error) {
+	var task *domain.Task
 	err := t.db.Transaction(ctx, nil, func(ctx context.Context) error {
 		_, err := t.taskStatusRepository.GetTaskStatus(ctx, taskStatusID, repository.LockTypeRecord)
 		if errors.Is(err, repository.ErrRecordNotFound) {
 			return errors.New("task status not found")
 		}
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to get task status: %w", err)
 		}
+
+		task = domain.NewTask(
+			values.NewTaskID(),
+			name,
+			description,
+			time.Now(),
+		)
+
+		err = t.taskRepository.CreateTask(ctx, taskStatusID, task)
+		if err != nil {
+			return fmt.Errorf("failed to create task: %w", err)
+		}
+
+		return nil
 	})
+	if err != nil {
+		return nil, fmt.Errorf("failed in transaction: %w", err)
+	}
+
+	return task, nil
 }
