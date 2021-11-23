3536aef06432c8fe43e28f348d066d70b793e757
mazrean <shunsuke.w04@gmail.com>
2021-11-23 19:56:34 +0900

    GetTask

--- repository/sqlx/task.go
+++ repository/sqlx/task.go
@@ -3,11 +3,21 @@ package sqlx
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/mazrean/todoList/domain"
 	"github.com/mazrean/todoList/domain/values"
+	"github.com/mazrean/todoList/repository"
 )
+
+type TaskTable struct {
+	ID           uuid.UUID `db:"id"`
+	TaskStatusID uuid.UUID `db:"task_status_id"`
+	Name         string    `db:"name"`
+	Description  string    `db:"description"`
+	CreatedAt    time.Time `db:"created_at"`
+}
 
 type Task struct {
 	db *DB
@@ -96,4 +106,35 @@ func (t *Task) DeleteTask(ctx context.Context, taskID values.TaskID) error {
 	}
 
 	return nil
+}
+
+func (t *Task) GetTask(ctx context.Context, taskID values.TaskID, lockType repository.LockType) (*domain.Task, error) {
+	db, err := t.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	query := "SELECT * FROM tasks WHERE id = ?"
+	switch lockType {
+	case repository.LockTypeRecord:
+		query += " FOR UPDATE"
+	}
+
+	taskTable := TaskTable{}
+	err = db.GetContext(
+		ctx,
+		&taskTable,
+		query,
+		uuid.UUID(taskID),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get task: %w", err)
+	}
+
+	return domain.NewTask(
+		values.NewTaskIDFromUUID(taskTable.ID),
+		values.NewTaskName(taskTable.Name),
+		values.NewTaskDescription(taskTable.Description),
+		taskTable.CreatedAt,
+	), nil
 }
