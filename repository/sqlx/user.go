284a0655d7d8696b25779ac453b22f70a6949654
mazrean <shunsuke.w04@gmail.com>
2021-11-23 18:45:33 +0900

    DeleteUser

--- repository/sqlx/user.go
+++ repository/sqlx/user.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/mazrean/todoList/domain"
+	"github.com/mazrean/todoList/domain/values"
 )
 
 type User struct {
@@ -53,6 +54,24 @@ func (u *User) UpdateUser(ctx context.Context, user *domain.User) error {
 	)
 	if err != nil {
 		return fmt.Errorf("failed to update user: %w", err)
+	}
+
+	return nil
+}
+
+func (u *User) DeleteUser(ctx context.Context, id values.UserID) error {
+	db, err := u.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	_, err = db.ExecContext(
+		ctx,
+		"UPDATE users SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?",
+		uuid.UUID(id),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to delete user: %w", err)
 	}
 
 	return nil
