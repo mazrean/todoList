6cc3533644bca153150898cf7371dece499bafc8
mazrean <shunsuke.w04@gmail.com>
2021-11-23 19:19:34 +0900

    CreateTaskStatus

--- repository/sqlx/task_status.go
+++ repository/sqlx/task_status.go
@@ -1,5 +1,12 @@
 package sqlx
 
+import (
+	"context"
+
+	"github.com/mazrean/todoList/domain"
+	"github.com/mazrean/todoList/domain/values"
+)
+
 type TaskStatus struct {
 	db *DB
 }
@@ -8,4 +15,7 @@ func NewTaskStatus(db *DB) *TaskStatus {
 	return &TaskStatus{
 		db: db,
 	}
+}
+
+func (ts *TaskStatus) CreateTaskStatus(ctx context.Context, dashboardID values.DashboardID, taskStatus *domain.TaskStatus) error {
 }
